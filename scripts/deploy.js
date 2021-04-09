0f999ac775af31f366d1cb5142f3d3aa87cd158b
seosigoto <seo.sigoto@gmail.com>
2021-04-09 01:12:32 -0700

    .

---
+++ scripts/deploy.js
@@ -0,0 +1,43 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy(                     
+      ["Leo", "Aang", "Pikachu"],       
+      ["https://i.imgur.com/pKd5Sdk.png", 
+      "https://i.imgur.com/xVu4vFL.png", 
+      "https://i.imgur.com/u7T87A6.png"],
+      [100, 200, 300],                    
+      [100, 50, 25],
+      "Elon Musk", // Boss name
+      "https://i.imgur.com/AksR0tt.png", // Boss image
+      10000, // Boss hp
+      50 // Boss attack damage                       
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+  
+    
+    let txn;
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+  
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+  
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+  
+    console.log("Done!");
+  
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
